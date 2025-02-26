de4a87e67498c1abd1acfc30b6544917d21253d8
jhajathanawit <jhajathanawit@gmail.com>
2025-02-26 12:55:16 +0700

    make random component look good

--- missAnime/src/components/random.tsx
+++ missAnime/src/components/random.tsx
@@ -1,4 +1,5 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 interface RandomAnime {
     mal_id: number;
@@ -31,15 +32,39 @@ export default function RandomAnime() {
     }
 
     return (
-        <div key={anime.mal_id} className='flex items-center gap-4'>
-            <div className='w-80 h-108'>
-                <img className='rounded-[10px] w-full h-full object-contain' src={anime.images.webp.large_image_url} alt={anime.title} />
-            </div>
-            <div className='flex flex-col gap-2 text-pink-100'>
-                <h2>{anime.title}</h2>
-                <p>Rank: {anime.rank}</p>
-                <p>Score: {anime.score}</p>
-            <button className='bg-pink-500' onClick={fetchRandomAnime}>Random</button>
+        <div className="relative p-4 w-full rounded-[10px]">
+            <div
+                className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-50 rounded-[10px]"
+                style={{ backgroundImage: `url(${anime.images.webp.large_image_url})` }}
+            ></div>
+            <div className="relative flex justify-center items-center gap-4">
+                <Link to={`/packages/${anime.mal_id}`} key={anime.mal_id} className="object-cover m-4">
+                    <div className="w-80 h-108 rounded-[10px] overflow-hidden">
+                        <img
+                            className="w-full h-full object-contain"
+                            src={anime.images.webp.large_image_url}
+                            alt={anime.title}
+                        />
+                    </div>
+                </Link>
+                <div className="flex flex-col gap-2 text-pink-100  text-2xl font-bold bg-[#1f293a50] p-4 rounded-[10px]">
+                    <h2>{anime.title}</h2>
+                    <p>Rank: {anime.rank}</p>
+                    <p>Score: {anime.score}</p>
+                    <div className='flex gap-4 justify-center'>
+                        <button className="bg-pink-500 rounded-[10px] p-1 hover:text-[#0f151f] hover:bg-pink-300" onClick={fetchRandomAnime}>
+                        Random
+                    </button>
+                    <Link to={`/packages/${anime.mal_id}`} key={anime.mal_id} className="object-cover">
+                     
+                        <button className="bg-pink-500 rounded-[10px]  p-1 hover:text-[#0f151f] hover:bg-pink-300" onClick={fetchRandomAnime}>
+                        detail
+                    </button>
+                    </Link>
+
+                    </div>
+                    
+                </div>
             </div>
         </div>
     );

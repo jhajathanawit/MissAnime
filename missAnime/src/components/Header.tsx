ee704f5771a3196d681adaf7310a612895c1d735
jhajathanawit <jhajathanawit@gmail.com>
2025-03-08 16:43:12 +0700

    navbar toggle ,link item in navbar

--- missAnime/src/components/Header.tsx
+++ missAnime/src/components/Header.tsx
@@ -1,26 +1,23 @@
 import { Link } from "react-router-dom"
 import SearchInput from "./SearchInput"
+import Navbar from "./navbar/navbar"
 
 export default function Header() {
     return (
-        <div className="flex-col justify-between items-center px-4 border-b h-14 py-2"> 
-           <div className="flex justify-between items-center">
+        <div className="flex-col justify-between items-center px-4 border-b py-2"> 
+           <div className="flex justify-between items-center py-2">
                 <div >
                     <Link to='/' className="flex text-4xl font-bold ">
                         <h1 className="text-pink-100">MISS</h1>
                         <h1 className="text-pink-500">ANIME</h1>
                     </Link>
                 </div>
-                <div className="flex gap-4">
-                    <ul className="flex gap-4 text-pink-100">
-                        <li>JikanAPI</li>
-                        <li>GitHub</li>
-                        <li>ContactUs</li>
-                    </ul>
+                <div >
+                    <Navbar/>
                 </div>
             </div> 
             
-            <div className="w-full max-w-xl ml-4 font-sans p-4">
+            <div className="w-full max-w-xl font-sans">
                 <SearchInput/>
             </div>
             
---
+++ missAnime/src/components/navbar/navbar.tsx
@@ -0,0 +1,68 @@
+import React,{ useState } from 'react';
+
+function Navbar(){
+
+    const[menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => { 
+        setMenuOpen(!menuOpen);
+        
+    } 
+
+    const[contact, setContact] = useState(false);
+    const toggleContact = () => {
+        setContact(!contact);
+    }
+
+    return (
+       <div className='text-white text-xl'>
+        <div className='text-white text-xl'>
+
+            <div className='md:hidden'>
+                <button id='menu-toggle' className='text-white' onClick={toggleMenu}>
+                    <svg className='w-6 h-6' fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                    </svg>
+                </button>
+            </div>
+
+            <ul className="hidden md:flex gap-4">
+                <li><a target='_blank' href="https://jikan.moe/">JikanAPI</a></li>
+                <li><a target='_blank' href="https://github.com/jhajathanawit/MissAnime">GitHub</a></li>
+                <button onClick={toggleContact}>contact
+                    {contact ? (
+                        <ul className='text-sm text-left'>
+                            <li><a target='_blank' href="https://www.facebook.com/thanawit.rungsangchan/">FaceBook</a></li>
+                            <li><a target='_blank' href="https://www.facebook.com/thanawit.rungsangchan/">Line</a></li>
+                            <li><a target='_blank' href="https://fastwork.co/user/thanawit.r">FastWork</a></li>
+                        </ul>
+                    ):null}
+                </button>
+                
+
+
+            </ul>
+        </div>
+        {menuOpen ? (<ul className="flex-col md:hidden gap-4">
+                <li><a target='_blank' href="https://jikan.moe/">JikanAPI</a></li>
+                <li><a target='_blank' href="https://github.com/jhajathanawit/MissAnime">GitHub</a></li>
+                <button onClick={toggleContact}>contact
+                    {contact ? (
+                        <ul className='text-sm text-left'>
+                            <li><a target='_blank' href="https://www.facebook.com/thanawit.rungsangchan/">FaceBook</a></li>
+                            <li><a target='_blank' href="https://www.facebook.com/thanawit.rungsangchan/">Line</a></li>
+                            <li><a target='_blank' href="https://fastwork.co/user/thanawit.r">FastWork</a></li>
+                        </ul>
+                    ):null}
+                </button>
+            </ul>
+            ):null}
+        
+       </div>
+        
+       
+
+    )
+}
+
+export default Navbar;
